783d9ba50994b30cfaa42cd785b7babab2698308
Konstantin S <havendv@gmail.com>
2025-03-11 06:39:39 +0400

    feat: Enhance TMA initialization with platform check and improved layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,24 @@
 
 import { useEffect, useState } from 'react';
 import { retrieveLaunchParams } from '@telegram-apps/sdk';
+import { isTMA } from '@telegram-apps/bridge';
 
 export default function Home() {
-  const [initData, setInitData] = useState<unknown>(null);
-  const [initDataRaw, setInitDataRaw] = useState<unknown>(null);
+  const [initData, setInitData] = useState<unknown>({});
+  const [initDataRaw, setInitDataRaw] = useState<unknown>({});
   useEffect(() => {
-    const { initDataRaw, initData } = retrieveLaunchParams();
-    setInitData(initData);
-    setInitDataRaw(initDataRaw);
+    if (isTMA()) {
+      const { initDataRaw, initData } = retrieveLaunchParams();
+      setInitData(initData);
+      setInitDataRaw(initDataRaw);
+    }
   }, []);
 
   return (
-    <div>
-      Hello, Telegram Mini App!
+    <div className="flex flex-col items-center justify-center h-screen">
+      Hello, Telegram Mini App! <br />
       {JSON.stringify(initData)}
+      <br />
       {JSON.stringify(initDataRaw)}
     </div>
   );
