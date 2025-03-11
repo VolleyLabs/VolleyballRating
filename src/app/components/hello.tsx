7c6f77943fc9609bc3fd95955723f8d8124889ed
Konstantin S <havendv@gmail.com>
2025-03-11 19:16:12 +0400

    feat: Display user profile photo in Hello component

--- src/app/components/hello.tsx
+++ src/app/components/hello.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react';
 import { retrieveLaunchParams } from '@telegram-apps/sdk';
 import { isTMA, RetrieveLPResult } from '@telegram-apps/bridge';
+import Image from 'next/image';
 
 export default function Hello() {
   const [launchParams, setLaunchParams] = useState<RetrieveLPResult | null>(null);
@@ -18,6 +19,9 @@ export default function Hello() {
   return (
     <div className="">
       <h1>Hello, {launchParams?.tgWebAppData?.user?.first_name}</h1>
+      {launchParams?.tgWebAppData?.user?.photo_url && (
+        <Image src={launchParams?.tgWebAppData?.user?.photo_url} alt="User Photo" width={100} height={100} />
+      )}
     </div>
   );
 }
